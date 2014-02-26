eb41dbc01f1e08777cea9d84fab51716af610381
larsbrubaker <larsbrubaker@gmail.com>
2014-02-26 11:05:58 -0800

    Putting in new release tests to make sure all sorts of things are good when we deploy. Have a new command line argument of test that will run release tests. Put in two tests to ensure that we don't have DEBUG defined when we ship release builds. Wrote a exception dumper that will output data when we have an error.

---
+++ Testing/ReleaseTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MatterHackers.MatterControl.Testing
+{
+    public static class ReleaseTests
+    {
+        public static void AssertDebugNotDefined()
+        {
+#if DEBUG
+            throw new Exception("DEBUG is defined and should not be!");
+#endif
+        }
+    }
+}
---
+++ Testing/TestingDispatch.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Diagnostics;
+
+namespace MatterHackers.MatterControl.Testing
+{
+    public class TestingDispatch
+    {
+        string errorLogFileName = null;
+        public TestingDispatch()
+        {
+            errorLogFileName = string.Format("ErrorLog - {0:yyyy-MM-dd hh-mmtt}.txt", DateTime.Now);
+            string firstLine = string.Format("MatterControl Errors: {0:yyyy-MM-dd hh:mm:ss tt}", DateTime.Now);
+            using (StreamWriter file = new StreamWriter(errorLogFileName))
+            {
+                file.WriteLine(errorLogFileName, firstLine);
+            }
+        }
+
+        public void RunTests(string[] testCommands)
+        {
+            try { ReleaseTests.AssertDebugNotDefined(); }
+            catch (Exception e) { DumpException(e); }
+
+            try { MatterHackers.GCodeVisualizer.GCodeFile.AssertDebugNotDefined(); }
+            catch (Exception e) { DumpException(e); }
+        }
+
+        void DumpException(Exception e)
+        {
+            using (StreamWriter w = File.AppendText(errorLogFileName))
+            {
+                w.WriteLine(e.Message);
+                w.Write(e.StackTrace);
+                w.WriteLine();
+            }
+        }
+    }
+}
