c7b4cd51cc60b066bd5742c2a1a144e176c4252a
larsbrubaker <larsbrubaker@gmail.com>
2014-05-11 20:15:41 -0700

    run it as a command.

--- SlicerConfiguration/SlicingQueue.cs
+++ SlicerConfiguration/SlicingQueue.cs
@@ -26,7 +26,7 @@ The views and conclusions contained in the software and documentation are those
 of the authors and should not be interpreted as representing official policies, 
 either expressed or implied, of the FreeBSD Project.
 */
-#define RUN_MATTER_SLICE_IN_PROCESS
+//#define RUN_MATTER_SLICE_IN_PROCESS
 
 using System;
 using System.IO;
