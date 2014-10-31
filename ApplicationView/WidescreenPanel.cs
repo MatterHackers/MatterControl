95b1367c424705092a9240e0f175234ee2741276
larsbrubaker <larsbrubaker@gmail.com>
2014-10-31 15:28:51 -0700

    turn off new mode

--- ApplicationView/WidescreenPanel.cs
+++ ApplicationView/WidescreenPanel.cs
@@ -26,7 +26,7 @@ The views and conclusions contained in the software and documentation are those
 of the authors and should not be interpreted as representing official policies, 
 either expressed or implied, of the FreeBSD Project.
 */
-#define NEW_TWO_COLUMN_MODE
+//#define NEW_TWO_COLUMN_MODE
 using System;
 using System.Collections.Generic;
 using System.Linq;
