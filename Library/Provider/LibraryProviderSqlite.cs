beffdda91e33064fd79bbb2c54096487ca0f7358
John Lewin <lewin76@gmail.com>
2015-11-10 13:00:54 -0800

    Drop file extension before performing string match
     - Fixes #107851156

--- Library/Provider/LibraryProviderSqlite.cs
+++ Library/Provider/LibraryProviderSqlite.cs
@@ -249,8 +249,8 @@ namespace MatterHackers.MatterControl.PrintLibrary.Provider
 
 			var existingLibaryItems = this.GetLibraryItems().Select(i => i.Name);
 
-			// Build a list of files that need to be imported into the library
-			var missingFiles = filenamesToValidate.Where(fileName => !existingLibaryItems.Contains(fileName, StringComparer.OrdinalIgnoreCase));
+			// Drop extensions and build a list of files that need to be imported into the library
+			var missingFiles = filenamesToValidate.Select(f => Path.GetFileNameWithoutExtension(f)).Where(fileName => !existingLibaryItems.Contains(fileName, StringComparer.OrdinalIgnoreCase));
 
 			// Create temp files on disk that can be imported into the library
 			var tempFilesToImport = missingFiles.Select(fileName =>
