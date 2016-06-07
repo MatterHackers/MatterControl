20a3cd7668c2c4872b8c45e37e5dbaa0d674ec72
John Lewin <lewin76@gmail.com>
2016-06-07 09:41:12 -0700

    Run validation tests on slice presets
     - Issue #800

--- Tests/MatterControl.Tests/MatterControl/ConfigIniTests.cs
+++ Tests/MatterControl.Tests/MatterControl/ConfigIniTests.cs
@@ -27,60 +27,78 @@ namespace MatterControl.Tests.MatterControl
 							   PrinterName = configIni.Directory.Name,
 							   Oem = configIni.Directory.Parent.Name,
 							   ConfigPath = configIni.FullName,
-							   RelativeConfigPath = configIni.FullName.Substring(printerSettingsDirectory.Length + 1),
-							   SettingsLayer = SettingsLayer.LoadFromIni(configIni.FullName)
+							   ConfigIni = new LayerInfo()
+							   {
+								   RelativePath = configIni.FullName.Substring(printerSettingsDirectory.Length + 1),
+								   Settings = SettingsLayer.LoadFromIni(configIni.FullName),
+							   },
+							   MatterialLayers = LoadLayers(Path.Combine(configIni.Directory.FullName, "material")),
+							   QualityLayers = LoadLayers(Path.Combine(configIni.Directory.FullName, "quality"))
 						   }).ToList();
+		}
+
+		private static List<LayerInfo> LoadLayers(string layersDirectory)
+		{
+			return Directory.Exists(layersDirectory) ?
+					Directory.GetFiles(layersDirectory, "*.slice").Select(file => new LayerInfo()
+					{
+						RelativePath = file.Substring(printerSettingsDirectory.Length + 1),
+						Settings = SettingsLayer.LoadFromIni(file)
+					}).ToList()
+					: new List<LayerInfo>();
 		}
 
 		[Test]
 		public void CsvBedSizeExistsAndHasTwoValues()
 		{
-			ValidateOnAllPrinters(printer =>
+			ValidateOnAllPrinters((printer, layer) =>
 			{
-				string bedSize = printer.SettingsLayer.ValueOrDefault("bed_size");
+
+
+				string bedSize = layer.Settings.ValueOrDefault("bed_size");
 
 				// Must exist in all configs
-				Assert.IsNotNullOrEmpty(bedSize, "[bed_size] must exist: " + printer.RelativeConfigPath);
+				Assert.IsNotNullOrEmpty(bedSize, "[bed_size] must exist: " + layer.RelativePath);
 
 				string[] segments = bedSize.Trim().Split(',');
 
 				// Must be a CSV and have two values
-				Assert.AreEqual(2, segments.Length, "[bed_size] should have two values separated by a comma: " + printer.RelativeConfigPath);
+				Assert.AreEqual(2, segments.Length, "[bed_size] should have two values separated by a comma: " + layer.RelativePath);
 			});
 		}
 
 		[Test]
 		public void CsvPrintCenterExistsAndHasTwoValues()
 		{
-			ValidateOnAllPrinters(printer =>
+			ValidateOnAllPrinters((printer, layer) =>
 			{
-				string printCenter = printer.SettingsLayer.ValueOrDefault("print_center");
+				string printCenter = layer.Settings.ValueOrDefault("print_center");
 
 				// Must exist in all configs
-				Assert.IsNotNullOrEmpty(printCenter, "[print_center] must exist: " + printer.RelativeConfigPath);
+				Assert.IsNotNullOrEmpty(printCenter, "[print_center] must exist: " + layer.RelativePath);
 
 				string[] segments = printCenter.Trim().Split(',');
 
 				// Must be a CSV and have only two values
-				Assert.AreEqual(2, segments.Length, "[print_center] should have two values separated by a comma: " + printer.RelativeConfigPath);
+				Assert.AreEqual(2, segments.Length, "[print_center] should have two values separated by a comma: " + layer.RelativePath);
 			});
 		}
 
 		[Test]
 		public void RetractLengthIsLessThanTwenty()
 		{
-			ValidateOnAllPrinters(printer =>
+			ValidateOnAllPrinters((printer, layer) =>
 			{
-				string retractLengthString = printer.SettingsLayer.ValueOrDefault("retract_length");
+				string retractLengthString = layer.Settings.ValueOrDefault("retract_length");
 				if (!string.IsNullOrEmpty(retractLengthString))
 				{
 					float retractLength;
 					if (!float.TryParse(retractLengthString, out retractLength))
 					{
-						Assert.Fail("Invalid [retract_length] value (float parse failed): " + printer.RelativeConfigPath);
+						Assert.Fail("Invalid [retract_length] value (float parse failed): " + layer.RelativePath);
 					}
 
-					Assert.Less(retractLength, 20, "[retract_length]: " + printer.RelativeConfigPath);
+					Assert.Less(retractLength, 20, "[retract_length]: " + layer.RelativePath);
 				}
 			});
 		}
@@ -88,19 +106,19 @@ namespace MatterControl.Tests.MatterControl
 		[Test]
 		public void ExtruderCountIsGreaterThanZero()
 		{
-			ValidateOnAllPrinters(printer =>
+			ValidateOnAllPrinters((printer, layer) =>
 			{
-				string extruderCountString = printer.SettingsLayer.ValueOrDefault("extruder_count");
+				string extruderCountString = layer.Settings.ValueOrDefault("extruder_count");
 				if (!string.IsNullOrEmpty(extruderCountString))
 				{
 					int extruderCount;
 					if (!int.TryParse(extruderCountString, out extruderCount))
 					{
-						Assert.Fail("Invalid [extruder_count] value (int parse failed): " + printer.RelativeConfigPath);
+						Assert.Fail("Invalid [extruder_count] value (int parse failed): " + layer.RelativePath);
 					}
 
 					// Must be greater than zero
-					Assert.Greater(extruderCount, 0, "[extruder_count]: " + printer.RelativeConfigPath);
+					Assert.Greater(extruderCount, 0, "[extruder_count]: " + layer.RelativePath);
 				}
 			});
 		}
@@ -108,20 +126,20 @@ namespace MatterControl.Tests.MatterControl
 		[Test]
 		public void MinFanSpeedOneHundredOrLess()
 		{
-			ValidateOnAllPrinters(printer =>
+			ValidateOnAllPrinters((printer, layer) =>
 			{
-				string fanSpeedString = printer.SettingsLayer.ValueOrDefault("min_fan_speed");
+				string fanSpeedString = layer.Settings.ValueOrDefault("min_fan_speed");
 				if (!string.IsNullOrEmpty(fanSpeedString))
 				{
 					// Must be valid int data
 					int minFanSpeed;
 					if (!int.TryParse(fanSpeedString, out minFanSpeed))
 					{
-						Assert.Fail("Invalid [min_fan_speed] value (int parse failed): " + printer.RelativeConfigPath);
+						Assert.Fail("Invalid [min_fan_speed] value (int parse failed): " + layer.RelativePath);
 					}
 
 					// Must be less than or equal to 100
-					Assert.LessOrEqual(minFanSpeed, 100, "[min_fan_speed]: " + printer.RelativeConfigPath);
+					Assert.LessOrEqual(minFanSpeed, 100, "[min_fan_speed]: " + layer.RelativePath);
 				}
 			});
 		}
@@ -129,20 +147,20 @@ namespace MatterControl.Tests.MatterControl
 		[Test]
 		public void MaxFanSpeedOneHundredOrLess()
 		{
-			ValidateOnAllPrinters(printer =>
+			ValidateOnAllPrinters((printer, layer) =>
 			{
-				string fanSpeedString = printer.SettingsLayer.ValueOrDefault("max_fan_speed");
+				string fanSpeedString = layer.Settings.ValueOrDefault("max_fan_speed");
 				if (!string.IsNullOrEmpty(fanSpeedString))
 				{
 					// Must be valid int data
 					int maxFanSpeed;
 					if (!int.TryParse(fanSpeedString, out maxFanSpeed))
 					{
-						Assert.Fail("Invalid [max_fan_speed] value (int parse failed): " + printer.RelativeConfigPath);
+						Assert.Fail("Invalid [max_fan_speed] value (int parse failed): " + layer.RelativePath);
 					}
 
 					// Must be less than or equal to 100
-					Assert.LessOrEqual(maxFanSpeed, 100, "[max_fan_speed]: " + printer.RelativeConfigPath);
+					Assert.LessOrEqual(maxFanSpeed, 100, "[max_fan_speed]: " + layer.RelativePath);
 				}
 			});
 		}
@@ -150,16 +168,16 @@ namespace MatterControl.Tests.MatterControl
 		[Test]
 		public void NoCurlyBracketsInGcode()
 		{
-			ValidateOnAllPrinters(printer =>
+			ValidateOnAllPrinters((printer, layer) =>
 			{
 				// TODO: Why aren't we testing all gcode sections?
 				string[] keysToTest = { "start_gcode", "end_gcode" };
 				foreach (string gcodeKey in keysToTest)
 				{
-					string gcode = printer.SettingsLayer.ValueOrDefault(gcodeKey);
+					string gcode = layer.Settings.ValueOrDefault(gcodeKey);
 					if (gcode.Contains("{") || gcode.Contains("}") )
 					{
-						Assert.Fail(string.Format("[{0}] Curly brackets not allowed: {1}", gcodeKey, printer.RelativeConfigPath));
+						Assert.Fail(string.Format("[{0}] Curly brackets not allowed: {1}", gcodeKey, layer.RelativePath));
 					}
 				}
 			});
@@ -168,9 +186,9 @@ namespace MatterControl.Tests.MatterControl
 		[Test]
 		public void BottomSolidLayersEqualsOneMM()
 		{
-			ValidateOnAllPrinters(printer =>
+			ValidateOnAllPrinters((printer, layer) =>
 			{
-				string bottomSolidLayers = printer.SettingsLayer.ValueOrDefault("bottom_solid_layers");
+				string bottomSolidLayers = layer.Settings.ValueOrDefault("bottom_solid_layers");
 				if (!string.IsNullOrEmpty(bottomSolidLayers))
 				{
 					if (bottomSolidLayers != "1mm")
@@ -179,7 +197,7 @@ namespace MatterControl.Tests.MatterControl
 						return;
 					}
 
-					Assert.AreEqual("1mm", bottomSolidLayers, "[bottom_solid_layers] must be 1mm: " + printer.RelativeConfigPath);
+					Assert.AreEqual("1mm", bottomSolidLayers, "[bottom_solid_layers] must be 1mm: " + layer.RelativePath);
 				}
 			});
 		}
@@ -187,40 +205,40 @@ namespace MatterControl.Tests.MatterControl
 		[Test]
 		public void NoFirstLayerTempInStartGcode()
 		{
-			ValidateOnAllPrinters(printer =>
+			ValidateOnAllPrinters((printer, layer) =>
 			{
-				string startGcode = printer.SettingsLayer.ValueOrDefault("start_gcode");
-				Assert.False(startGcode.Contains("first_layer_temperature"), "[start_gcode] should not contain [first_layer_temperature]" + printer.RelativeConfigPath);
+				string startGcode = layer.Settings.ValueOrDefault("start_gcode");
+				Assert.False(startGcode.Contains("first_layer_temperature"), "[start_gcode] should not contain [first_layer_temperature]" + layer.RelativePath);
 			});
 		}
 
 		[Test]
 		public void NoFirstLayerBedTempInStartGcode()
 		{
-			ValidateOnAllPrinters(printer =>
+			ValidateOnAllPrinters((printer, layer) =>
 			{
-				string startGcode = printer.SettingsLayer.ValueOrDefault("start_gcode");
-				Assert.False(startGcode.Contains("first_layer_bed_temperature"), "[start_gcode] should not contain [first_layer_bed_temperature]" + printer.RelativeConfigPath);
+				string startGcode = layer.Settings.ValueOrDefault("start_gcode");
+				Assert.False(startGcode.Contains("first_layer_bed_temperature"), "[start_gcode] should not contain [first_layer_bed_temperature]" + layer.RelativePath);
 			});
 		}
 
 		[Test]
 		public void FirstLayerHeightLessThanNozzleDiameterXExtrusionMultiplier()
 		{
-			ValidateOnAllPrinters(printer =>
+			ValidateOnAllPrinters((printer, layer) =>
 			{
-				if (printer.SettingsLayer.ValueOrDefault("output_only_first_layer") == "1")
+				if (layer.Settings.ValueOrDefault("output_only_first_layer") == "1")
 				{
 					return;
 				}
 
-				float nozzleDiameter = float.Parse(printer.SettingsLayer.ValueOrDefault("nozzle_diameter"));
-				float layerHeight = float.Parse(printer.SettingsLayer.ValueOrDefault("layer_height"));
+				float nozzleDiameter = float.Parse(layer.Settings.ValueOrDefault("nozzle_diameter"));
+				float layerHeight = float.Parse(layer.Settings.ValueOrDefault("layer_height"));
 
 
 				float firstLayerExtrusionWidth;
 
-				string firstLayerExtrusionWidthString = printer.SettingsLayer.ValueOrDefault("first_layer_extrusion_width");
+				string firstLayerExtrusionWidthString = layer.Settings.ValueOrDefault("first_layer_extrusion_width");
 				if (!string.IsNullOrEmpty(firstLayerExtrusionWidthString) && firstLayerExtrusionWidthString.Trim() != "0")
 				{
 					firstLayerExtrusionWidth = ValueOrPercentageOf(firstLayerExtrusionWidthString, nozzleDiameter);
@@ -230,7 +248,7 @@ namespace MatterControl.Tests.MatterControl
 					firstLayerExtrusionWidth = nozzleDiameter;
 				}
 
-				string firstLayerHeightString = printer.SettingsLayer.ValueOrDefault("first_layer_height");
+				string firstLayerHeightString = layer.Settings.ValueOrDefault("first_layer_height");
 				if (!string.IsNullOrEmpty(firstLayerHeightString))
 				{
 					float firstLayerHeight = ValueOrPercentageOf(firstLayerHeightString, layerHeight);
@@ -244,7 +262,7 @@ namespace MatterControl.Tests.MatterControl
 						return;
 					}
 
-					Assert.Less(firstLayerHeight, minimumLayerHeight, "[first_layer_height] must be less than [firstLayerExtrusionWidth]: " + printer.RelativeConfigPath);
+					Assert.Less(firstLayerHeight, minimumLayerHeight, "[first_layer_height] must be less than [firstLayerExtrusionWidth]: " + layer.RelativePath);
 				}
 				
 			});
@@ -253,15 +271,15 @@ namespace MatterControl.Tests.MatterControl
 		[Test]
 		public void LayerHeightLessThanNozzleDiameter()
 		{
-			ValidateOnAllPrinters(printer =>
+			ValidateOnAllPrinters((printer, layer) =>
 			{
-				if (printer.SettingsLayer.ValueOrDefault("output_only_first_layer") == "1")
+				if (layer.Settings.ValueOrDefault("output_only_first_layer") == "1")
 				{
 					return;
 				}
 
-				float nozzleDiameter = float.Parse(printer.SettingsLayer.ValueOrDefault("nozzle_diameter"));
-				float layerHeight = float.Parse(printer.SettingsLayer.ValueOrDefault("layer_height"));
+				float nozzleDiameter = float.Parse(layer.Settings.ValueOrDefault("nozzle_diameter"));
+				float layerHeight = float.Parse(layer.Settings.ValueOrDefault("layer_height"));
 
 				double minimumLayerHeight = nozzleDiameter * 0.85;
 
@@ -272,18 +290,18 @@ namespace MatterControl.Tests.MatterControl
 					return;
 				}
 
-				Assert.Less(layerHeight, minimumLayerHeight, "[layer_height] must be less than [minimumLayerHeight]: " + printer.RelativeConfigPath);
+				Assert.Less(layerHeight, minimumLayerHeight, "[layer_height] must be less than [minimumLayerHeight]: " + layer.RelativePath);
 			});
 		}
 
 		[Test]
 		public void FirstLayerExtrusionWidthGreaterThanNozzleDiameterIfSet()
 		{
-			ValidateOnAllPrinters(printer =>
+			ValidateOnAllPrinters((printer, layer) =>
 			{
-				float nozzleDiameter = float.Parse(printer.SettingsLayer.ValueOrDefault("nozzle_diameter"));
+				float nozzleDiameter = float.Parse(layer.Settings.ValueOrDefault("nozzle_diameter"));
 
-				string firstLayerExtrusionWidthString = printer.SettingsLayer.ValueOrDefault("first_layer_extrusion_width");
+				string firstLayerExtrusionWidthString = layer.Settings.ValueOrDefault("first_layer_extrusion_width");
 				if (!string.IsNullOrEmpty(firstLayerExtrusionWidthString))
 				{
 					float firstLayerExtrusionWidth = ValueOrPercentageOf(firstLayerExtrusionWidthString, nozzleDiameter);
@@ -293,7 +311,7 @@ namespace MatterControl.Tests.MatterControl
 						return;
 					}
 
-					Assert.GreaterOrEqual(firstLayerExtrusionWidth, nozzleDiameter, "[first_layer_extrusion_width] must be nozzle diameter or greater: " + printer.RelativeConfigPath);
+					Assert.GreaterOrEqual(firstLayerExtrusionWidth, nozzleDiameter, "[first_layer_extrusion_width] must be nozzle diameter or greater: " + layer.RelativePath);
 				}
 			});
 		}
@@ -301,12 +319,12 @@ namespace MatterControl.Tests.MatterControl
 		[Test]
 		public void SupportMaterialAssignedToExtruderOne()
 		{
-			ValidateOnAllPrinters(printer =>
+			ValidateOnAllPrinters((printer, layer) =>
 			{
-				string supportMaterialExtruder = printer.SettingsLayer.ValueOrDefault("support_material_extruder");
+				string supportMaterialExtruder = layer.Settings.ValueOrDefault("support_material_extruder");
 				if (!string.IsNullOrEmpty(supportMaterialExtruder) && printer.Oem != "Esagono")
 				{
-					Assert.AreEqual("1", supportMaterialExtruder, "[support_material_extruder] must be assigned to extruder 1: " + printer.RelativeConfigPath);
+					Assert.AreEqual("1", supportMaterialExtruder, "[support_material_extruder] must be assigned to extruder 1: " + layer.RelativePath);
 				}
 			});
 		}
@@ -314,9 +332,9 @@ namespace MatterControl.Tests.MatterControl
 		[Test]
 		public void SupportInterfaceMaterialAssignedToExtruderOne()
 		{
-			ValidateOnAllPrinters(printer =>
+			ValidateOnAllPrinters((printer, layer) =>
 			{
-				string supportMaterialInterfaceExtruder = printer.SettingsLayer.ValueOrDefault("support_material_interface_extruder");
+				string supportMaterialInterfaceExtruder = layer.Settings.ValueOrDefault("support_material_interface_extruder");
 				if (!string.IsNullOrEmpty(supportMaterialInterfaceExtruder) && printer.Oem != "Esagono")
 				{
 					Assert.AreEqual("1", supportMaterialInterfaceExtruder, "[support_material_interface_extruder] must be assigned to extruder 1");
@@ -342,18 +360,43 @@ namespace MatterControl.Tests.MatterControl
 		/// config.ini loaded into a SettingsLayer as well as state about the printer
 		/// </summary>
 		/// <param name="action">The action to invoke for each printer</param>
-		private void ValidateOnAllPrinters(Action<PrinterConfig> action)
+		private void ValidateOnAllPrinters(Action<PrinterConfig, LayerInfo> action)
 		{
 			var ruleViolations = new List<string>();
 
 			foreach (var printer in allPrinters)
 			{
 				printer.RuleViolated = false;
-				action(printer);
+
+				action(printer, printer.ConfigIni);
 
 				if (printer.RuleViolated)
 				{
-					ruleViolations.Add(printer.RelativeConfigPath);
+					ruleViolations.Add(printer.ConfigIni.RelativePath);
+				}
+
+				foreach (var layer in printer.MatterialLayers)
+				{
+					printer.RuleViolated = false;
+
+					action(printer, layer);
+
+					if (printer.RuleViolated)
+					{
+						ruleViolations.Add(layer.RelativePath);
+					}
+				}
+
+				foreach (var layer in printer.QualityLayers)
+				{
+					printer.RuleViolated = false;
+
+					action(printer, layer);
+
+					if (printer.RuleViolated)
+					{
+						ruleViolations.Add(layer.RelativePath);
+					}
 				}
 			}
 
@@ -367,14 +410,21 @@ namespace MatterControl.Tests.MatterControl
 			public string PrinterName { get; set; }
 			public string Oem { get; set; }
 			public string ConfigPath { get; set; }
-			public string RelativeConfigPath { get; set; }
-			public SettingsLayer SettingsLayer { get; set; }
+			public LayerInfo ConfigIni { get; set; }
 
 			// HACK: short term hack to support a general purpose test rollup function for cases where multiple config files 
 			// violate a rule and in the short term we want to report and resolve the issues in batch rather than having a 
 			// single test failure. Long term the single test failure better communicates the issue and assist with troubleshooting
 			// by using  .AreEqual .LessOrEqual, etc. to communicate intent
 			public bool RuleViolated { get; set; } = false;
+			public List<LayerInfo> MatterialLayers { get; internal set; }
+			public List<LayerInfo> QualityLayers { get; internal set; }
+		}
+
+		private class LayerInfo
+		{
+			public string RelativePath { get; set; }
+			public SettingsLayer Settings { get; set; }
 		}
 	}
 }
