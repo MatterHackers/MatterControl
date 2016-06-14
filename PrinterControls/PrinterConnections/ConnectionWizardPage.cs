a9f3cbf4c46c0abf4aabc86c79835620edb648b8
John Lewin <lewin76@gmail.com>
2016-06-14 11:28:13 -0700

    Remove ActivePrinter variable from ConnectionWizard
     - Remove excess properties from PrinterInfo
     - Remove call to Parent.Close from onPrinterStatusChanged

--- PrinterControls/PrinterConnections/ConnectionWizardPage.cs
+++ PrinterControls/PrinterConnections/ConnectionWizardPage.cs
@@ -10,47 +10,9 @@ namespace MatterHackers.MatterControl.PrinterControls.PrinterConnections
 {
 	public class ConnectionWizardPage : WizardPage
 	{
-		private PrinterInfo activePrinter;
-
 		public ConnectionWizardPage() : base("Cancel")
 		{
 			cancelButton.Click += (s, e) => PrinterConnectionAndCommunication.Instance.HaltConnectionThread();
-		}
-
-		public PrinterInfo ActivePrinter
-		{
-			get
-			{
-				if (activePrinter == null)
-				{
-					var settings = ActiveSliceSettings.Instance;
-					activePrinter = new PrinterInfo
-					{
-						AutoConnect = settings.DoAutoConnect(),
-						BaudRate = settings.BaudRate(),
-						ComPort = settings.ComPort(),
-						DriverType = settings.DriverType(),
-						Id = settings.ID,
-						Name = settings.Name()
-					};
-				}
-
-				return activePrinter;
-			}
-		}
-
-		protected void SaveAndExit()
-		{
-			ActiveSliceSettings.Instance.RunInTransaction(settings =>
-			{
-				settings.SetAutoConnect(ActivePrinter.AutoConnect);
-				settings.SetBaudRate(ActivePrinter.BaudRate);
-				settings.SetComPort(ActivePrinter.ComPort);
-				settings.SetDriverType(ActivePrinter.DriverType);
-				settings.SetName(ActivePrinter.Name);
-			});
-
-			UiThread.RunOnIdle(WizardWindow.Close);
 		}
 	}
 }
