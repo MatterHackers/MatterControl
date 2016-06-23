bc5ef8a5b397a7a098cf3b6957945ecd84c58c54
Matt Moening <matt.moening@matterhackers.com>
2016-06-22 18:05:52 -0700

    Implemented MC side receive of Public Profile list and public profiles

---
+++ Tests/MatterControl.AutomationTests/RetrievePublicProfileTest.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using MatterHackers.MatterControl.VersionManagement;
+using System.IO;
+using MatterHackers.MatterControl.DataStorage;
+using MatterHackers.MatterControl.SettingsManagement;
+using System.Threading;
+using MatterHackers.MatterControl.Tests.Automation;
+using MatterHackers.Agg.UI.Tests;
+using MatterHackers.GuiAutomation;
+using MatterHackers.Agg.UI;
+using Newtonsoft.Json;
+using MatterHackers.Agg.PlatformAbstract;
+
+namespace MatterControl.Tests.MatterControl
+{
+	[TestFixture, RunInApplicationDomain]
+	public class RetrievePublicProfileTest
+	{
+		private string deviceToken = null;
+
+		[Test,RunInApplicationDomain]
+		public void RetrievePrinterProfileListWorking()
+		{
+
+			StaticData.Instance = new MatterHackers.Agg.FileSystemStaticData(Path.Combine("..", "..", "..", "..", "StaticData"));
+
+			string profilePath = Path.Combine(ApplicationDataStorage.ApplicationUserDataPath, "data", "temp", "cache", "profiles", "oemprofiles.json");
+			if(File.Exists(profilePath))
+			{
+				File.Delete(profilePath);
+			}
+			//MatterControlUtilities.OverrideAppDataLocation();
+			AutoResetEvent requestCompleteWaiter = new AutoResetEvent(false);
+			PublicProfilesRequest retrieveProfiles = new PublicProfilesRequest();
+			retrieveProfiles.URI = "https://mattercontrol-test.appspot.com/api/1/device/get-public-profile-list";
+
+
+			retrieveProfiles.RequestComplete += (sender, eArgs) => { requestCompleteWaiter.Set(); };
+
+			retrieveProfiles.Request();
+			Assert.IsTrue(requestCompleteWaiter.WaitOne());
+
+			Assert.IsTrue(File.Exists(profilePath));
+
+			//Call Retrieve Profile next
+			RetrievePrinterProfileWorking();
+		}
+
+		//[Test,Category("CloudProfiles")]
+		public void RetrievePrinterProfileWorking()
+		{
+			string make = OemSettings.Instance.OemProfiles.First().Key;
+			string model = OemSettings.Instance.OemProfiles[make].First().Key;
+			deviceToken = OemSettings.Instance.OemProfiles[make][model];
+			string expectedProfilePath = Path.Combine(ApplicationDataStorage.ApplicationUserDataPath, "Profiles", string.Format("{0}.json", deviceToken));
+			if (File.Exists(expectedProfilePath))
+			{
+				File.Delete(expectedProfilePath);
+			}
+			RetrievePublicProfileRequest request = new RetrievePublicProfileRequest();
+			RetrievePublicProfileRequest.DownloadBaseUrl = "https://mattercontrol-test.appspot.com/api/1/device/get-public-profile";
+			string recievedPrinterProfile = request.getPrinterProfileByMakeModel(make,model);
+			RetrievePublicProfileRequest.DownloadBaseUrl = "https://mattercontrol.appspot.com/api/1/device/get-public-profile";
+
+			Assert.IsNotNullOrEmpty(recievedPrinterProfile);
+			//Assert.AreEqual(expectedProfilePath, recievedProfilePath,"Recieved Profile path does not match expected path.");
+			//Assert.IsTrue(File.Exists(expectedProfilePath));
+		}
+
+		
+	}
+}
---
+++ VersionManagement/RetrievePublicProfileRequest.cs
@@ -0,0 +1,67 @@
+﻿using MatterHackers.MatterControl.DataStorage;
+using MatterHackers.MatterControl.SettingsManagement;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace MatterHackers.MatterControl.VersionManagement
+{
+	class RetrievePublicProfileRequest
+	{
+		internal static string DownloadBaseUrl { get; set; }
+
+		public RetrievePublicProfileRequest()
+		{
+#if DEBUG
+			DownloadBaseUrl = "https://mattercontrol-test.appspot.com/api/1/device/get-public-profile";
+#else
+			DownloadBaseUrl = "https://mattercontrol.appspot.com/api/1/device/get-public-profile";
+#endif
+		}
+
+		public string getPrinterProfileByMakeModel(string make, string model)
+		{
+			string deviceToken = OemSettings.Instance.OemProfiles[make][model];
+			string profiletext = DownloadPrinterProfile(deviceToken);
+			return profiletext;
+		}
+
+		internal static string DownloadPrinterProfile(string deviceToken)
+		{
+			// TODO: Enable caching
+			//Keept track of version. When retrieving check version
+
+			string url = DownloadBaseUrl + string.Format("/{0}",deviceToken);
+
+			string profilePath = Path.Combine(ApplicationDataStorage.ApplicationUserDataPath,"Profiles",string.Format("{0}.json",deviceToken));
+			WebClient client = new WebClient();
+			string profileText = client.DownloadString(url);
+			//File.WriteAllText(profileText, profilePath);
+
+			return profileText;
+			//HttpClient client = new HttpClient();
+
+			//Get a pemporaty path to write to during download. If download completes without error we move this file to the proper path
+			//string tempFilePath = ApplicationDataStorage.Instance.GetTempFileName(".json");
+
+			//byte[] buffer = new byte[65536];
+			//using (var writeStream = File.Create(tempFilePath))
+			//using (var instream = await client.GetStreamAsync(url))
+			//{
+			//	int bytesRead = await instream.ReadAsync(buffer, 0, buffer.Length);
+			//	while(bytesRead != 0)
+			//	{
+			//		writeStream.Write(buffer, 0, bytesRead);
+
+			//		bytesRead = await instream.ReadAsync(buffer, 0, buffer.Length);
+			//	}
+			//}
+
+			//File.Move(tempFilePath, profilePath);
+
+			//return profilePath;
+		}
+		//Used in test to access test server before changes go onto live server
+	}
+}
