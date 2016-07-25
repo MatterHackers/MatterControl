4d84b11706c9b94a331dd95a22c459ce5fb13691
Matt Moening <matt.moening@matterhackers.com>
2016-07-25 15:56:03 -0700

    Fixed Capitalization errors in oemProfiles.json

--- SettingsManagement/OemSettings.cs
+++ SettingsManagement/OemSettings.cs
@@ -128,7 +128,7 @@ namespace MatterHackers.MatterControl.SettingsManagement
 		private void Deserialized(StreamingContext context)
 		{
 			// Load from StaticData to prepopulate oemProfiles for when user create a printer before load cacheable is done
-			OemProfiles = JsonConvert.DeserializeObject<OemProfileDictionary>(StaticData.Instance.ReadAllText(Path.Combine("Profiles", "oemprofiles.json")));
+			OemProfiles = JsonConvert.DeserializeObject<OemProfileDictionary>(StaticData.Instance.ReadAllText(Path.Combine("Profiles", "oemProfiles.json")));
 
 			var manufacturesList = OemProfiles.Keys.ToDictionary(oem => oem).ToList();
 			SetManufacturers(manufacturesList);
@@ -143,7 +143,7 @@ namespace MatterHackers.MatterControl.SettingsManagement
 				}
 
 				var oemProfiles = await ApplicationController.LoadCacheableAsync<OemProfileDictionary>(
-					"oemprofiles.json",
+					"oemProfiles.json",
 					"profiles",
 					ApplicationController.GetPublicProfileList);
 
