9f9fe17843624f0bebff95d470959102083416c1
Lars Brubaker <larsbrubaker@gmail.com>
2016-08-08 14:53:10 -0700

    This save was already happening.

--- SlicerConfiguration/Settings/SettingsHelpers.cs
+++ SlicerConfiguration/Settings/SettingsHelpers.cs
@@ -239,8 +239,6 @@ namespace MatterHackers.MatterControl.SlicerConfiguration
 		{
 			printLevelingData = data;
 			printerSettings.SetValue("print_leveling_data", JsonConvert.SerializeObject(data));
-			// and persist them to disk
-			printerSettings.Save();
 		}
 
 		public void DoPrintLeveling(bool doLeveling)
