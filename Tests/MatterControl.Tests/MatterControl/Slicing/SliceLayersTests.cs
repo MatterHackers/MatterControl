c16cede87acbee130c80a69e433f61b7ec8492bb
Lars Brubaker <larsbrubaker@gmail.com>
2016-09-28 10:24:11 -0700

    Getting android to build

--- Tests/MatterControl.Tests/MatterControl/Slicing/SliceLayersTests.cs
+++ Tests/MatterControl.Tests/MatterControl/Slicing/SliceLayersTests.cs
@@ -28,7 +28,10 @@ either expressed or implied, of the FreeBSD Project.
 */
 
 using MatterHackers.Agg.PlatformAbstract;
+using MatterHackers.MatterSlice.Tests;
+#if !__ANDROID__
 using MatterHackers.MatterControl.Tests.Automation;
+#endif
 using MatterHackers.PolygonMesh;
 using MatterHackers.PolygonMesh.Processors;
 using MatterHackers.VectorMath;
