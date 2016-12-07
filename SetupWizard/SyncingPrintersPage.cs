0b88351365d99c3549bbd93b1ace69ade241f6c2
Lars Brubaker <larsbrubaker@gmail.com>
2016-12-07 14:53:48 -0800

    Only close if we don't need to select a printer

--- SetupWizard/SyncingPrintersPage.cs
+++ SetupWizard/SyncingPrintersPage.cs
@@ -34,8 +34,9 @@ namespace MatterHackers.MatterControl.SetupWizard
 				{
 					//Set as active printer
 					ActiveSliceSettings.SwitchToProfile(ProfileManager.Instance.ActiveProfiles.First().ID);
+					// only close the window if we are not switching to the setup printer form
+					UiThread.RunOnIdle(WizardWindow.Close);
 				}
-				UiThread.RunOnIdle(WizardWindow.Close);
 			});
 			footerRow.AddChild(new HorizontalSpacer());
 			footerRow.AddChild(cancelButton);
