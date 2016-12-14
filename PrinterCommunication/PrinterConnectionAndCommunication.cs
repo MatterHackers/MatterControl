bf62f924209c6ae758ca888f4e6179798aaece64
Lars Brubaker <larsbrubaker@gmail.com>
2016-12-14 15:03:37 -0800

    Fixed resend to work on smoothie
    MatterHackers/MatterControl#1700
    Not sending resends to Smoothieware

--- PrinterCommunication/PrinterConnectionAndCommunication.cs
+++ PrinterCommunication/PrinterConnectionAndCommunication.cs
@@ -265,8 +265,8 @@ namespace MatterHackers.MatterControl.PrinterCommunication
 
 			ReadLineContainsCallBacks.AddCallbackToKey("T:", ReadTemperatures);
 
-			ReadLineContainsCallBacks.AddCallbackToKey("rs ", PrinterRequestsResend); // smoothie is lower case and no :
-			ReadLineContainsCallBacks.AddCallbackToKey("RS:", PrinterRequestsResend);
+			ReadLineStartCallBacks.AddCallbackToKey("rs ", PrinterRequestsResend); // smoothie is lower case and no :
+			ReadLineStartCallBacks.AddCallbackToKey("RS:", PrinterRequestsResend);
 			ReadLineContainsCallBacks.AddCallbackToKey("Resend:", PrinterRequestsResend);
 
 			ReadLineContainsCallBacks.AddCallbackToKey("FIRMWARE_NAME:", PrinterStatesFirmware);
@@ -1470,17 +1470,22 @@ namespace MatterHackers.MatterControl.PrinterCommunication
 		{
 			FoundStringEventArgs foundStringEventArgs = e as FoundStringEventArgs;
 
-			string[] splitOnColon = foundStringEventArgs.LineToCheck.Split(':');
-
-			if (splitOnColon.Length > 1)
+			if (foundStringEventArgs != null
+				&& !string.IsNullOrEmpty(foundStringEventArgs.LineToCheck))
 			{
-				int result = 0;
-				if (int.TryParse(splitOnColon[1], out result))
+				string line = foundStringEventArgs.LineToCheck;
+				// marlin and repetier send a : before the number and then and ok
+				if (!GCodeFile.GetFirstNumberAfter(":", line, ref currentLineIndexToSend))
 				{
-					currentLineIndexToSend = result;
+					// smoothie sends an N before the number and no ok
+					if (GCodeFile.GetFirstNumberAfter("N", line, ref currentLineIndexToSend))
+					{
+						// clear waiting for ok because smoothie will not send it
+						PrintingCanContinue(null, null);
+					}
 				}
 
-				if(currentLineIndexToSend >= allCheckSumLinesSent.Count)
+				if (currentLineIndexToSend >= allCheckSumLinesSent.Count)
 				{
 					SendLineToPrinterNow("M110 N1");
 				}
