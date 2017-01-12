51541abfa2e77df97b4b483d8e3793bbe710016a
Lars Brubaker <larsbrubaker@gmail.com>
2017-01-11 17:18:42 -0800

    turn off debug code

--- PrinterCommunication/PrinterConnectionAndCommunication.cs
+++ PrinterCommunication/PrinterConnectionAndCommunication.cs
@@ -2824,7 +2824,7 @@ namespace MatterHackers.MatterControl.PrinterCommunication
 		}
 
 		// this is to make it misbehave, chaos monkey, bad checksum
-		int checkSumCount = 1;
+		//int checkSumCount = 1;
 		private void WriteChecksumLineToPrinter(string lineToWrite)
 		{
 			SetDetailedPrintingState(lineToWrite);
@@ -2851,8 +2851,8 @@ namespace MatterHackers.MatterControl.PrinterCommunication
 			
 			allCheckSumLinesSent.Add(lineWithChecksum);
 
-			if ((checkSumCount++ % 11) == 0)
-				lineWithChecksum = lineWithCount + "*" + (GCodeFile.CalculateChecksum(lineWithCount) + checkSumCount).ToString();
+			//if ((checkSumCount++ % 11) == 0)
+				//lineWithChecksum = lineWithCount + "*" + (GCodeFile.CalculateChecksum(lineWithCount) + checkSumCount).ToString();
 
 			WriteRawToPrinter(lineWithChecksum + "\n", lineToWrite);
 		}
