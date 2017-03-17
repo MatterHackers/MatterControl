874af0e21bc66fb4da4275722499a35fd23e6973
John Lewin <lewin76@gmail.com>
2017-03-17 11:56:42 -0700

    Add RaygunMaxNotifications and enforce per instance reporting limits

--- MatterControlApplication.cs
+++ MatterControlApplication.cs
@@ -82,9 +82,11 @@ namespace MatterHackers.MatterControl
 
 		private Stopwatch totalDrawTime = new Stopwatch();
 
-#if true//!DEBUG
+		private const int RaygunMaxNotifications = 15;
+
+		private static int raygunNotificationCount = 0;
+
 		private static RaygunClient _raygunClient = GetCorrectClient();
-#endif
 
 		private static RaygunClient GetCorrectClient()
 		{
@@ -406,7 +408,10 @@ namespace MatterHackers.MatterControl
 			if (string.IsNullOrEmpty(channel) || channel != "release" || OemSettings.Instance.WindowTitleExtra == "Experimental")
 			{
 #if !DEBUG
-				_raygunClient.Send(e);
+				if (raygunNotificationCount++ < RaygunMaxNotifications)
+				{
+					_raygunClient.Send(e);
+				}
 #endif
 			}
 		}
@@ -517,14 +522,20 @@ namespace MatterHackers.MatterControl
 		private static void Application_ThreadException(object sender, ThreadExceptionEventArgs e)
 		{
 #if !DEBUG
-			_raygunClient.Send(e.Exception);
+			if(raygunNotificationCount++ < RaygunMaxNotifications)
+			{
+				_raygunClient.Send(e.Exception);
+			}
 #endif
 		}
 
 		private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
 		{
 #if !DEBUG
-			_raygunClient.Send(e.ExceptionObject as Exception);
+			if(raygunNotificationCount++ < RaygunMaxNotifications)
+			{
+				_raygunClient.Send(e.ExceptionObject as Exception);
+			}
 #endif
 		}
 
