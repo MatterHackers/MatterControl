32713e682c035e1dd6075f9bd837781deaef09d6
John Lewin <lewin76@gmail.com>
2017-06-15 17:10:36 -0700

    Sanitized Master.txt

--- PrinterControls/PrinterConnections/SetupStepComPortManual.cs
+++ PrinterControls/PrinterConnections/SetupStepComPortManual.cs
@@ -92,7 +92,7 @@ namespace MatterHackers.MatterControl.PrinterControls.PrinterConnections
 			printerComPortHelpLink.VAnchor = VAnchor.ParentBottom;
 			printerComPortHelpLink.Click += (s, e) => printerComPortHelpMessage.Visible = !printerComPortHelpMessage.Visible;
 
-			printerComPortHelpMessage = new TextWidget("The 'Serial Port' section lists all available serial\nports on your device. Changing which USB port the printer\nis conneted to may change the associated serial port.\n\nTip: If you are uncertain, unplug/plug in your printer\nand hit refresh. The new port that appears should be\nyour printer.".Localize(), 0, 0, 10);
+			printerComPortHelpMessage = new TextWidget("The 'Serial Port' section lists all available serial\nports on your device. Changing which USB port the printer\nis connected to may change the associated serial port.\n\nTip: If you are uncertain, unplug/plug in your printer\nand hit refresh. The new port that appears should be\nyour printer.".Localize(), 0, 0, 10);
 			printerComPortHelpMessage.TextColor = ActiveTheme.Instance.PrimaryTextColor;
 			printerComPortHelpMessage.Margin = new BorderDouble(top: 10);
 			printerComPortHelpMessage.Visible = false;
