88b8777d97f1f282b97f44a1c9311d5fbb970d05
John Lewin <lewin76@gmail.com>
2017-08-06 10:44:27 -0700

    Remove TouchscreenView

--- ApplicationView/ApplicationController.cs
+++ ApplicationView/ApplicationController.cs
@@ -803,22 +803,7 @@ namespace MatterHackers.MatterControl
 						// Accessing any property on ProfileManager will run the static constructor and spin up the ProfileManager instance
 						bool na = ProfileManager.Instance.IsGuestProfile;
 
-						// TODO: Short-term workaround to force DesktopView on Android
-#if __ANDROID__
-						if (false)
-#else
-						if (UserSettings.Instance.IsTouchScreen)
-#endif
-						{
-							// and make sure we have the check for print recovery wired up needed for lazy tabs.
-							var temp2 = PrintHistoryData.Instance;
-							// now bulid the ui
-							globalInstance.MainView = new TouchscreenView();
-						}
-						else
-						{
-							globalInstance.MainView = new DesktopView();
-						}
+						globalInstance.MainView = new DesktopView();
 
 						ActiveSliceSettings.ActivePrinterChanged.RegisterEvent((s, e) => ApplicationController.Instance.ReloadAll(), ref globalInstance.unregisterEvents);
 					}
