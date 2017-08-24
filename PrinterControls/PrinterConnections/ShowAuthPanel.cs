44069d280557bc54781ef815198a6143a33baa95
John Lewin <lewin76@gmail.com>
2017-08-23 23:59:45 -0700

    Organize usings

--- PrinterControls/PrinterConnections/ShowAuthPanel.cs
+++ PrinterControls/PrinterConnections/ShowAuthPanel.cs
@@ -27,12 +27,11 @@ of the authors and should not be interpreted as representing official policies,
 either expressed or implied, of the FreeBSD Project.
 */
 
-using System;
-using MatterHackers.Agg.UI;
-using MatterHackers.MatterControl.CustomWidgets;
-using MatterHackers.Localizations;
-using MatterHackers.MatterControl.SlicerConfiguration;
 using System.Linq;
+using MatterHackers.Agg.UI;
+using MatterHackers.Localizations;
+using MatterHackers.MatterControl.CustomWidgets;
+using MatterHackers.MatterControl.SlicerConfiguration;
 
 namespace MatterHackers.MatterControl.PrinterControls.PrinterConnections
 {
