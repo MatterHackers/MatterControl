8818d47b9de9cc4ba6f3dfb39c2e247648dd0470
John Lewin <lewin76@gmail.com>
2017-09-14 23:11:24 -0700

    Improve test teardown

--- Tests/MatterControl.Tests/MatterControl/SliceSettingsFieldTests.cs
+++ Tests/MatterControl.Tests/MatterControl/SliceSettingsFieldTests.cs
@@ -1,8 +1,38 @@
-﻿using System;
+﻿/*
+Copyright (c) 2017, John Lewin
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+The views and conclusions contained in the software and documentation are those
+of the authors and should not be interpreted as representing official policies,
+either expressed or implied, of the FreeBSD Project.
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 using MatterHackers.Agg;
 using MatterHackers.Agg.Platform;
 using MatterHackers.Agg.UI;
@@ -19,7 +49,7 @@ namespace MatterControl.Tests.MatterControl
 	{
 		public static Task RunTest(this SystemWindow systemWindow, AutomationTest automationTest, int timeout)
 		{
-			return AutomationRunner.ShowWindowAndExecuteTests(systemWindow, automationTest, timeout);
+			return AutomationRunner.ShowWindowAndExecuteTests(systemWindow, automationTest, timeout, closeWindow: () => Application.Exit());
 		}
 
 		public static void Add(this List<ValueMap> valueMap, string input, string expected)
@@ -28,7 +58,7 @@ namespace MatterControl.Tests.MatterControl
 		}
 	}
 
-	[TestFixture, Category("SliceSettingsTests")]
+	[TestFixture, Category("SliceSettingsTests"), RunInApplicationDomain]
 	public class SliceSettingsFieldTests
 	{
 		[Test]
@@ -99,7 +129,6 @@ namespace MatterControl.Tests.MatterControl
 					{"-abc", "0"},
 				});
 		}
-
 
 		[Test, Ignore("Not Implemented")]
 		public void IntFieldTest()
@@ -224,8 +253,5 @@ namespace MatterControl.Tests.MatterControl
 			}, 30);
 
 		}
-
 	}
-
-
 }
