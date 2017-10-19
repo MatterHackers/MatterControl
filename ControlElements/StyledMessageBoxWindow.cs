040361a4284b83a13a82bcf75714f23990179389
John Lewin <lewin76@gmail.com>
2017-10-18 22:50:57 -0700

    Convert StyledMessageBox over to WizardPage

--- ControlElements/StyledMessageBoxWindow.cs
+++ ControlElements/StyledMessageBoxWindow.cs
@@ -28,24 +28,15 @@ either expressed or implied, of the FreeBSD Project.
 */
 
 using System;
-using MatterHackers.Agg;
 using MatterHackers.Agg.Font;
 using MatterHackers.Agg.UI;
 using MatterHackers.Localizations;
-using MatterHackers.MatterControl.CustomWidgets;
 
 namespace MatterHackers.MatterControl
 {
-	public class StyledMessageBox : SystemWindow
+	public static class StyledMessageBox
 	{
-		private string unwrappedMessage;
-		private TextWidget messageContainer;
-		private FlowLayoutWidget middleRowContainer;
-
-		private Action<bool> responseCallback;
-
 		public enum MessageType { OK, YES_NO };
-		private double extraTextScaling = 1;
 
 		public static void ShowMessageBox(string message, string caption, MessageType messageType = MessageType.OK, string yesOk = "", string noCancel = "")
 		{
@@ -59,174 +50,106 @@ namespace MatterHackers.MatterControl
 
 		public static void ShowMessageBox(Action<bool> callback, string message, string caption, GuiWidget[] extraWidgetsToAdd, MessageType messageType, string yesOk = "", string noCancel = "")
 		{
-			StyledMessageBox messageBox = new StyledMessageBox(callback, message, caption, messageType, extraWidgetsToAdd, 400, 300, yesOk, noCancel);
-			messageBox.CenterInParent = true;
-			messageBox.ShowAsSystemWindow();
+			WizardWindow.Show(
+				new MessageBoxPage(callback, message, caption, messageType, extraWidgetsToAdd, 400, 300, yesOk, noCancel, ApplicationController.Instance.Theme));
 		}
 
-		public StyledMessageBox(Action<bool> callback, string message, string windowTitle, MessageType messageType, GuiWidget[] extraWidgetsToAdd, double width, double height, string yesOk, string noCancel)
-			: base(width, height)
+		private class MessageBoxPage : WizardPage
 		{
-			if (UserSettings.Instance.IsTouchScreen)
-			{
-				extraTextScaling = 1.33333;
-			}
+			private string unwrappedMessage;
+			private TextWidget messageContainer;
+			private Action<bool> responseCallback;
 
-			if (yesOk == "")
+			private double extraTextScaling = (UserSettings.Instance.IsTouchScreen) ? 1.33333 : 1;
+
+			public MessageBoxPage(Action<bool> callback, string message, string caption, MessageType messageType, GuiWidget[] extraWidgetsToAdd, double width, double height, string yesOk, string noCancel, ThemeConfig theme)
+				: base((noCancel == "") ? "No" : noCancel)
 			{
-				if (messageType == MessageType.OK)
+				this.WindowSize = new VectorMath.Vector2(width, height);
+
+				if (yesOk == "")
 				{
-					yesOk = "Ok".Localize();
+					yesOk = (messageType == MessageType.OK) ? "Ok".Localize() : "Yes".Localize();
 				}
-				else
+
+				this.HeaderText = caption;
+				this.IsModal = true;
+
+				responseCallback = callback;
+				unwrappedMessage = message;
+
+				contentRow.AddChild(messageContainer = new TextWidget(message, textColor: ActiveTheme.Instance.PrimaryTextColor, pointSize: 12 * extraTextScaling)
 				{
-					yesOk = "Yes".Localize();
-				}
-			}
-			if (noCancel == "")
-			{
-				noCancel = "No".Localize();
-			}
+					AutoExpandBoundsToText = true,
+					HAnchor = HAnchor.Left
+				});
 
-			responseCallback = callback;
-			unwrappedMessage = message;
-			FlowLayoutWidget topToBottom = new FlowLayoutWidget(FlowDirection.TopToBottom);
-			topToBottom.AnchorAll();
-			if (UserSettings.Instance.IsTouchScreen)
-			{
-				topToBottom.Padding = new BorderDouble(12, 12, 13, 8);
-			}
-			else
-			{
-				topToBottom.Padding = new BorderDouble(3, 0, 3, 5);
-			}
-
-			// Creates Header
-			FlowLayoutWidget headerRow = new FlowLayoutWidget(FlowDirection.LeftToRight);
-			headerRow.HAnchor = HAnchor.Stretch;
-			headerRow.Margin = new BorderDouble(0, 3, 0, 0);
-			headerRow.Padding = new BorderDouble(0, 3, 0, 3);
-			BackgroundColor = ActiveTheme.Instance.PrimaryBackgroundColor;
-
-			//Creates Text and adds into header
-			{
-				TextWidget elementHeader = new TextWidget(windowTitle, pointSize: 14 * extraTextScaling);
-				elementHeader.TextColor = ActiveTheme.Instance.PrimaryTextColor;
-				elementHeader.HAnchor = HAnchor.Stretch;
-				elementHeader.VAnchor = Agg.UI.VAnchor.Bottom;
-
-				headerRow.AddChild(elementHeader);
-				topToBottom.AddChild(headerRow);
-			}
-
-			// Creates container in the middle of window
-			middleRowContainer = new FlowLayoutWidget(FlowDirection.TopToBottom);
-			{
-				middleRowContainer.HAnchor = HAnchor.Stretch;
-				middleRowContainer.VAnchor = VAnchor.Stretch;
-				// normally the padding for the middle container should be just (5) all around. The has extra top space
-				middleRowContainer.Padding = new BorderDouble(5, 5, 5, 15);
-				middleRowContainer.BackgroundColor = ActiveTheme.Instance.SecondaryBackgroundColor;
-			}
-
-			messageContainer = new TextWidget(message, textColor: ActiveTheme.Instance.PrimaryTextColor, pointSize: 12 * extraTextScaling);
-			messageContainer.AutoExpandBoundsToText = true;
-			messageContainer.HAnchor = Agg.UI.HAnchor.Left;
-			middleRowContainer.AddChild(messageContainer);
-
-			if (extraWidgetsToAdd != null)
-			{
-				foreach (GuiWidget widget in extraWidgetsToAdd)
+				if (extraWidgetsToAdd != null)
 				{
-					middleRowContainer.AddChild(widget);
-				}
-			}
-
-			topToBottom.AddChild(middleRowContainer);
-
-			//Creates button container on the bottom of window
-			FlowLayoutWidget buttonRow = new FlowLayoutWidget(FlowDirection.LeftToRight);
-			{
-				BackgroundColor = ActiveTheme.Instance.PrimaryBackgroundColor;
-				buttonRow.HAnchor = HAnchor.Stretch;
-				buttonRow.Padding = new BorderDouble(0, 3);
-			}
-
-			var textImageButtonFactory = ApplicationController.Instance.Theme.ButtonFactory;
-
-			switch (messageType)
-			{
-				case MessageType.YES_NO:
+					foreach (GuiWidget widget in extraWidgetsToAdd)
 					{
-						Title = "MatterControl - " + "Please Confirm".Localize();
-						Button yesButton = textImageButtonFactory.Generate(yesOk);
-						yesButton.Name = "Yes Button";
-						yesButton.Click += okButton_Click;
-						yesButton.Cursor = Cursors.Hand;
-						buttonRow.AddChild(yesButton);
-
-						buttonRow.AddChild(new HorizontalSpacer());
-
-						Button noButton = textImageButtonFactory.Generate(noCancel);
-						noButton.Name = "No Button";
-						noButton.Click += noButton_Click;
-						noButton.Cursor = Cursors.Hand;
-						buttonRow.AddChild(noButton);
+						contentRow.AddChild(widget);
 					}
-					break;
+				}
 
-				case MessageType.OK:
-					{
-						Title = "MatterControl - " + "Alert".Localize();
-						Button okButton = textImageButtonFactory.Generate(yesOk);
-						okButton.Name = "Ok Button";
-						okButton.Cursor = Cursors.Hand;
-						okButton.Click += okButton_Click;
-						buttonRow.AddChild(okButton);
-					}
-					break;
+				Button affirmativeButton = textImageButtonFactory.Generate(yesOk);
+				affirmativeButton.Click += (s, e) =>
+				{
+					UiThread.RunOnIdle(this.WizardWindow.Close);
 
-				default:
-					throw new NotImplementedException();
+					// If applicable, invoke the callback
+					responseCallback?.Invoke(true);
+				};
+
+				this.AddPageAction(affirmativeButton);
+
+				switch (messageType)
+				{
+					case MessageType.YES_NO:
+						this.WindowTitle = "MatterControl - " + "Please Confirm".Localize();
+						affirmativeButton.Name = "Yes Button";
+						this.SetCancelButtonName("No Button");
+						break;
+
+					case MessageType.OK:
+						this.WindowTitle = "MatterControl - " + "Alert".Localize();
+						affirmativeButton.Name = "Ok Button";
+						this.HideCancelButton();
+						break;
+
+					default:
+						throw new NotImplementedException();
+				}
+
+				this.AdjustTextWrap();
 			}
 
-			topToBottom.AddChild(buttonRow);
-			this.AddChild(topToBottom);
-
-			IsModal = true;
-			AdjustTextWrap();
-		}
-
-		public override void OnBoundsChanged(EventArgs e)
-		{
-			AdjustTextWrap();
-			base.OnBoundsChanged(e);
-		}
-
-		private void AdjustTextWrap()
-		{
-			if (messageContainer != null)
+			public override void OnBoundsChanged(EventArgs e)
 			{
-				double wrappingSize = middleRowContainer.Width - (middleRowContainer.Padding.Width + messageContainer.Margin.Width);
-				if (wrappingSize > 0)
+				AdjustTextWrap();
+				base.OnBoundsChanged(e);
+			}
+
+			private void AdjustTextWrap()
+			{
+				if (messageContainer != null)
 				{
-					EnglishTextWrapping wrapper = new EnglishTextWrapping(12 * extraTextScaling * GuiWidget.DeviceScale);
-					string wrappedMessage = wrapper.InsertCRs(unwrappedMessage, wrappingSize);
-					messageContainer.Text = wrappedMessage;
+					double wrappingSize = contentRow.Width - (contentRow.Padding.Width + messageContainer.Margin.Width);
+					if (wrappingSize > 0)
+					{
+						var wrapper = new EnglishTextWrapping(12 * extraTextScaling * GuiWidget.DeviceScale);
+						messageContainer.Text = wrapper.InsertCRs(unwrappedMessage, wrappingSize);
+					}
 				}
 			}
+
+			protected override void OnCancel(out bool abortCancel)
+			{
+				responseCallback?.Invoke(false);
+				base.OnCancel(out abortCancel);
+			}
 		}
 
-		private void noButton_Click(object sender, EventArgs mouseEvent)
-		{
-			UiThread.RunOnIdle(Close);
-			responseCallback?.Invoke(false);
-		}
-
-		private void okButton_Click(object sender, EventArgs mouseEvent)
-		{
-			UiThread.RunOnIdle(Close);
-			responseCallback?.Invoke(true);
-		}
 	}
 }
+ 
