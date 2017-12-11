15b668e9fb266d61b6ae76aa3272ac8d3f6afe54
John Lewin <lewin76@gmail.com>
2017-12-11 15:50:01 -0800

    Add missing files

---
+++ PartPreviewWindow/View3D/PrinterBar/SliceButton.cs
@@ -0,0 +1,79 @@
+﻿/*
+Copyright (c) 2017, Lars Brubaker, John Lewin
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+The views and conclusions contained in the software and documentation are those
+of the authors and should not be interpreted as representing official policies,
+either expressed or implied, of the FreeBSD Project.
+*/
+
+using MatterHackers.Agg.UI;
+using MatterHackers.Localizations;
+using MatterHackers.MatterControl.CustomWidgets;
+using MatterHackers.MatterControl.PrinterCommunication;
+using System;
+
+namespace MatterHackers.MatterControl.PartPreviewWindow
+{
+	public class SliceButton : TextButton
+	{
+		private PrinterConfig printer;
+		private EventHandler unregisterEvents;
+
+		public SliceButton(PrinterConfig printer, ThemeConfig theme)
+			: base("Slice".Localize(), theme)
+		{
+			this.printer = printer;
+
+			printer.Connection.CommunicationStateChanged.RegisterEvent((s, e) =>
+			{
+				UiThread.RunOnIdle(SetButtonStates);
+			}, ref unregisterEvents);
+
+			SetButtonStates();
+		}
+
+		public override void OnClosed(ClosedEventArgs e)
+		{
+			unregisterEvents?.Invoke(this, null);
+			base.OnClosed(e);
+		}
+
+		protected void SetButtonStates()
+		{
+			switch (printer.Connection.CommunicationState)
+			{
+				case CommunicationStates.PreparingToPrint:
+				case CommunicationStates.PrintingFromSd:
+				case CommunicationStates.Printing:
+				case CommunicationStates.Paused:
+					this.Enabled = false;
+					break;
+
+				default:
+					this.Enabled = true;
+					break;
+			}
+		}
+	}
+}
