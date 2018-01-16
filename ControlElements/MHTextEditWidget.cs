cc339c6c751d2644b7b36ba997487643bfda27dd
LarsBrubaker <larsbrubaker@gmail.com>
2018-01-16 07:40:30 -0800

    Need Text Edit Widget outer scroll container to be stretch

--- ControlElements/MHTextEditWidget.cs
+++ ControlElements/MHTextEditWidget.cs
@@ -50,7 +50,6 @@ namespace MatterHackers.MatterControl
 
 			this.ActualTextEditWidget = new TextEditWidget(text, x, y, pointSize, pixelWidth, pixelHeight, multiLine, tabIndex: tabIndex, typeFace: typeFace)
 			{
-				HAnchor = HAnchor.Fit,
 				VAnchor = VAnchor.Bottom
 			};
 			this.ActualTextEditWidget.MinimumSize = new Vector2(Math.Max(ActualTextEditWidget.MinimumSize.X, pixelWidth), Math.Max(ActualTextEditWidget.MinimumSize.Y, pixelHeight));
