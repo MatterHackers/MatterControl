acd8842f0bb24f42a09d002c7ffdc6603b9b171e
John Lewin <lewin76@gmail.com>
2018-05-09 15:26:24 -0700

    Update paths for new location

--- MatterControl.Printing/SerialHelper/build.sh
+++ MatterControl.Printing/SerialHelper/build.sh
@@ -3,9 +3,9 @@ gcc -m32 -shared -fPIC SetSerial.c -o libSetSerial.so -v
 gcc -shared -fPIC SetSerial.c -o libSetSerial -v
 
 # Create path if needed
-mkdir -p ../../../../bin/Debug/
-mkdir -p ../../../../bin/Release/
+mkdir -p ../../bin/Debug/
+mkdir -p ../../bin/Release/
 
 # Copy to MatterControl build directories
-cp libSetSerial* ../../../../bin/Debug/
-cp libSetSerial* ../../../../bin/Release/
+cp libSetSerial* ../../bin/Debug/
+cp libSetSerial* ../../bin/Release/
