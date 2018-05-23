bfc3eddb064b77ad9b4085deab958748b05bf31c
John Lewin <lewin76@gmail.com>
2018-05-23 11:48:14 -0700

    Simplify

--- CustomWidgets/WizardControl.cs
+++ CustomWidgets/WizardControl.cs
@@ -27,12 +27,12 @@ of the authors and should not be interpreted as representing official policies,
 either expressed or implied, of the FreeBSD Project.
 */
 
+using System;
+using System.Collections.Generic;
 using MatterHackers.Agg;
 using MatterHackers.Agg.UI;
 using MatterHackers.Localizations;
 using MatterHackers.MatterControl.CustomWidgets;
-using System;
-using System.Collections.Generic;
 
 namespace MatterHackers.MatterControl
 {
@@ -142,7 +142,8 @@ namespace MatterHackers.MatterControl
 			AddChild(topToBottom);
 		}
 
-		IEnumerator<WizardControlPage> pagesCache;
+		private IEnumerator<WizardControlPage> pagesCache;
+
 		public override void Initialize()
 		{
 			if(pagesCache == null)
