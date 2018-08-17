69a9e90fcf13c59659f0008fa221e25bdf5ecbb2
LarsBrubaker <larsbrubaker@gmail.com>
2018-08-16 23:05:24 -0700

    Putting in more dual extrusion and wipe tower control

--- SlicerConfiguration/Slicer.cs
+++ SlicerConfiguration/Slicer.cs
@@ -53,7 +53,7 @@ namespace MatterHackers.MatterControl.SlicerConfiguration
 		private static Dictionary<Mesh, MeshPrintOutputSettings> meshPrintOutputSettings = new Dictionary<Mesh, MeshPrintOutputSettings>();
 
 		public static List<bool> extrudersUsed = new List<bool>();
-		public static bool runInProcess = false;
+		public static bool runInProcess = true;
 
 		public static List<(Matrix4X4 matrix, string fileName)> GetStlFileLocations(IObject3D reloadedItem, ref string mergeRules, PrinterConfig printer, IProgress<ProgressStatus> progressReporter, CancellationToken cancellationToken)
 		{
