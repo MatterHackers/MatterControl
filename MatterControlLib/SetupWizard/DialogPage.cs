45b9da7cf665660ef4c373e9bf9fe5ffe18816a5
John Lewin <lewin76@gmail.com>
2018-10-20 23:09:43 -0700

    Switch around colors to match styling on main form

--- MatterControlLib/SetupWizard/DialogPage.cs
+++ MatterControlLib/SetupWizard/DialogPage.cs
@@ -61,7 +61,6 @@ namespace MatterHackers.MatterControl
 
 			this.HAnchor = HAnchor.Stretch;
 			this.VAnchor = VAnchor.Stretch;
-			this.BackgroundColor = theme.ActiveTabColor;
 
 			if (cancelButtonText == null)
 			{
@@ -110,7 +109,7 @@ namespace MatterHackers.MatterControl
 			contentRow = new FlowLayoutWidget(FlowDirection.TopToBottom)
 			{
 				Padding = new BorderDouble(10),
-				BackgroundColor = theme.MinimalShade,
+				BackgroundColor = theme.ActiveTabColor,
 				HAnchor = HAnchor.Stretch,
 				VAnchor = VAnchor.Stretch
 			};
