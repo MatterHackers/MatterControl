8c1c0bcff429cf8367c7fce6562eb9dd6d647f27
Lars Brubaker <larsbrubaker@gmail.com>
2018-11-01 14:07:21 -0700

    Improving setup connection wizard
    issue: MatterHackers/MCCentral#4453
    Add images to connection wizard

--- MatterControlLib/CustomWidgets/TreeView/TreeView.cs
+++ MatterControlLib/CustomWidgets/TreeView/TreeView.cs
@@ -228,11 +228,13 @@ namespace MatterHackers.MatterControl.CustomWidgets
 				{
 					OnBeforeSelect(null);
 
+					// if the current selection (before change) is !null than clear its background color
 					if (_selectedNode != null)
 					{
 						_selectedNode.HighlightRegion.BackgroundColor = Color.Transparent;
 					}
 
+					// change the selection
 					_selectedNode = value;
 
 					if (_selectedNode != null)
