eaabcccea8ecd8c3f95a22750b6f3244e656cc0a
LarsBrubaker <larsbrubaker@gmail.com>
2018-11-11 19:31:00 -0800

    Put in a first pass of an improved boolean function based on libigl

--- MatterControlLib/PartPreviewWindow/View3D/Actions/SubtractObject3D.cs
+++ MatterControlLib/PartPreviewWindow/View3D/Actions/SubtractObject3D.cs
@@ -31,11 +31,15 @@ using MatterHackers.Agg;
 using MatterHackers.Agg.UI;
 using MatterHackers.DataConverters3D;
 using MatterHackers.Localizations;
+using MatterHackers.MatterControl.DataStorage;
 using MatterHackers.MatterControl.DesignTools;
 using MatterHackers.PolygonMesh;
+using MatterHackers.PolygonMesh.Processors;
 using MatterHackers.VectorMath;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -83,21 +87,13 @@ namespace MatterHackers.MatterControl.PartPreviewWindow.View3D
 
 						progressStatus.Status = "Do CSG";
 						reporter?.Report(progressStatus);
-						transformedKeep = PolygonMesh.Csg.CsgOperations.Subtract(transformedKeep, transformedRemove, (status, progress0To1) =>
-						{
-							// Abort if flagged
-							cancellationToken.ThrowIfCancellationRequested();
-
-							progressStatus.Status = status;
-							progressStatus.Progress0To1 = percentCompleted + amountPerOperation * progress0To1;
-							reporter?.Report(progressStatus);
-						}, cancellationToken);
+						var result = SingleSubtract(transformedKeep, transformedRemove, reporter, amountPerOperation, percentCompleted, progressStatus, cancellationToken);
 						var inverse = keep.matrix.Inverted;
-						transformedKeep.Transform(inverse);
+						result.Transform(inverse);
 
 						using (keep.obj3D.RebuildLock())
 						{
-							keep.obj3D.Mesh = transformedKeep;
+							keep.obj3D.Mesh = result;
 						}
 
 						percentCompleted += amountPerOperation;
@@ -108,6 +104,60 @@ namespace MatterHackers.MatterControl.PartPreviewWindow.View3D
 					remove.obj3D.Visible = false;
 				}
 			}
+		}
+
+		private static Mesh SingleSubtract(Mesh transformedKeep, Mesh transformedRemove, IProgress<ProgressStatus> reporter, double amountPerOperation, double percentCompleted, ProgressStatus progressStatus, CancellationToken cancellationToken)
+		{
+			var libiglExe = "libigl_boolean.exe";
+			if (File.Exists(libiglExe)
+				&& sizeof(int) == 8) // only try to run the improved booleans if we are 64 bit and it is there
+			{
+				string folderToSaveStlsTo = Path.Combine(ApplicationDataStorage.Instance.ApplicationTempDataPath, "amf_to_stl");
+				// Create directory if needed
+				Directory.CreateDirectory(folderToSaveStlsTo);
+
+				string stlFileA = Path.Combine(folderToSaveStlsTo, Path.ChangeExtension(Path.GetRandomFileName(), ".stl"));
+				StlProcessing.Save(transformedKeep, stlFileA, CancellationToken.None);
+
+				string stlFileB = Path.Combine(folderToSaveStlsTo, Path.ChangeExtension(Path.GetRandomFileName(), ".stl"));
+				StlProcessing.Save(transformedRemove, stlFileB, CancellationToken.None);
+
+				string stlFileResult = Path.Combine(folderToSaveStlsTo, Path.ChangeExtension(Path.GetRandomFileName(), ".stl"));
+
+				// if we have the libigl_boolean.exe
+				var slicerProcess = new Process()
+				{
+					StartInfo = new ProcessStartInfo()
+					{
+						Arguments = "{0} {1} {2} -".FormatWith(stlFileA, stlFileB, stlFileResult),
+						CreateNoWindow = true,
+						WindowStyle = ProcessWindowStyle.Hidden,
+						RedirectStandardError = true,
+						RedirectStandardOutput = true,
+						FileName = libiglExe,
+						UseShellExecute = false
+					}
+				};
+				slicerProcess.Start();
+				slicerProcess.WaitForExit();
+
+				// load up the 
+				var result = StlProcessing.Load(stlFileResult, CancellationToken.None);
+				if(result != null)
+				{
+					return result;
+				}
+			}
+
+			return PolygonMesh.Csg.CsgOperations.Subtract(transformedKeep, transformedRemove, (status, progress0To1) =>
+			{
+				// Abort if flagged
+				cancellationToken.ThrowIfCancellationRequested();
+
+				progressStatus.Status = status;
+				progressStatus.Progress0To1 = percentCompleted + amountPerOperation * progress0To1;
+				reporter?.Report(progressStatus);
+			}, cancellationToken);
 		}
 
 		public override void OnInvalidate(InvalidateArgs invalidateType)
