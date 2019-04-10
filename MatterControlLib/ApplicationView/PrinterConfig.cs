6e40e6b17bbc20ef3f7132b1c6d88815ddbfc9cd
jlewin <lewin76@gmail.com>
2019-04-10 16:48:36 -0700

    Add HotendBounds to Bed
    - Ideally would be in PrinterSettings but dependencies are in
      printer.
    - Long term Bed bounds and Hotend bounds should move to settings

--- MatterControlLib/ApplicationView/PrinterConfig.cs
+++ MatterControlLib/ApplicationView/PrinterConfig.cs
@@ -444,7 +444,9 @@ namespace MatterHackers.MatterControl
 			macroReplacements = null;
 
 			// Dispose children
+			this.Bed.Dispose();
 			this.Connection.Dispose();
+
 			this.Disposed?.Invoke(this, null);
 			this.Disposed = null;
 		}
