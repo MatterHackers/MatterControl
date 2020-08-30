cf3a6d5893e28af2680979918218f189b476717a
LarsBrubaker <larsbrubaker@gmail.com>
2020-08-30 09:49:23 -0700

    Don't double scale on high rez

--- MatterControlLib/Library/Widgets/LibraryWidget.cs
+++ MatterControlLib/Library/Widgets/LibraryWidget.cs
@@ -482,8 +482,8 @@ namespace MatterHackers.MatterControl.PrintLibrary
 				null,
 				containerItem,
 				null,
-				(int)Math.Round(20 * DeviceScale),
-				(int)Math.Round(20 * DeviceScale),
+				20,
+				20,
 				theme).ConfigureAwait(false);
 
 			treeNode.ExpandedChanged += (s, e) =>
