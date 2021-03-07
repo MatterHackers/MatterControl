550d96645ff41787d0b9b6e6cfd0ee58ae1d0a87
LarsBrubaker <larsbrubaker@gmail.com>
2021-03-06 22:34:13 -0800

    Fixing more tests

--- MatterControlLib/PrinterCommunication/Drivers/Emulator/Emulator.cs
+++ MatterControlLib/PrinterCommunication/Drivers/Emulator/Emulator.cs
@@ -335,7 +335,7 @@ namespace MatterHackers.PrinterEmulator
 Commands:
     SLOW // make the emulator simulate actual printing speeds (default)
     FAST // run as fast as possible
-	THROWERROR // generate a simulated error for testing
+    THROWERROR // generate a simulated error for testing
 Emulating:
 FIRMWARE_NAME:Marlin V1; Sprinter/grbl mashup for gen6 FIRMWARE_URL:https://github.com/MarlinFirmware/Marlin PROTOCOL_VERSION:1.0 MACHINE_TYPE:Framelis v1 EXTRUDER_COUNT:1 UUID:155f84b5-d4d7-46f4-9432-667e6876f37a
 ok
