1b715c53474210c4b6665b689aa5ef1af856601c
LarsBrubaker <larsbrubaker@gmail.com>
2022-05-27 07:47:21 -0700

    Adding holes test and fixing regression

--- MatterControlLib/PartPreviewWindow/View3D/Actions/CombineObject3D_2.cs
+++ MatterControlLib/PartPreviewWindow/View3D/Actions/CombineObject3D_2.cs
@@ -129,10 +129,10 @@ namespace MatterHackers.MatterControl.PartPreviewWindow.View3D
             SourceContainer.Visible = true;
             RemoveAllButSource();
 
-            var holes = SourceContainer.VisibleMeshes().Where(m => m.OutputType == PrintOutputTypes.Hole);
+            var holes = SourceContainer.VisibleMeshes().Where(m => m.WorldOutputType(SourceContainer) == PrintOutputTypes.Hole);
 
             Mesh resultsMesh = null;
-            var participants = SourceContainer.VisibleMeshes().Where(m => m.OutputType != PrintOutputTypes.Hole);
+            var participants = SourceContainer.VisibleMeshes().Where(m => m.WorldOutputType(SourceContainer) != PrintOutputTypes.Hole);
             if (participants.Count() == 0)
             {
                 if (holes.Count() == 0)
@@ -157,7 +157,8 @@ namespace MatterHackers.MatterControl.PartPreviewWindow.View3D
                 {
                     var holesItem = new Object3D()
                     {
-                        Mesh = holesMesh
+                        Mesh = holesMesh,
+                        OutputType = PrintOutputTypes.Hole
                     };
 
                     if (resultsMesh != null)
