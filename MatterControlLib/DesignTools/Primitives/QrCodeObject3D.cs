6618dd11ed799b7ed1b735d4957d7351c3cf101b
Lars Brubaker <larsbrubaker@gmail.com>
2023-03-30 13:24:50 -0700

    Working on improving qrcodes

--- MatterControlLib/DesignTools/Primitives/QrCodeObject3D.cs
+++ MatterControlLib/DesignTools/Primitives/QrCodeObject3D.cs
@@ -304,7 +304,39 @@ namespace MatterHackers.MatterControl.DesignTools
 			return destImage;
 		}
 
-		public bool ConvertBitmapToImage(ImageBuffer destImage, System.Drawing.Bitmap bitmap)
+        public static System.Drawing.Bitmap ConvertImageToBitmap(ImageBuffer sourceImage)
+        {
+            var bitmap = new System.Drawing.Bitmap(sourceImage.Width, sourceImage.Height, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+
+            var bitmapData = bitmap.LockBits(new System.Drawing.Rectangle(0, 0, bitmap.Width, bitmap.Height), System.Drawing.Imaging.ImageLockMode.ReadWrite, bitmap.PixelFormat);
+
+            int destIndex = 0;
+            unsafe
+            {
+                byte[] sourceBuffer = sourceImage.GetBuffer();
+                byte* pDestBuffer = (byte*)bitmapData.Scan0;
+                int scanlinePadding = bitmapData.Stride - bitmapData.Width * 4;
+                for (int y = 0; y < sourceImage.Height; y++)
+                {
+                    int sourceIndex = sourceImage.GetBufferOffsetXY(0, sourceImage.Height - 1 - y);
+                    for (int x = 0; x < sourceImage.Width; x++)
+                    {
+                        pDestBuffer[destIndex++] = sourceBuffer[sourceIndex++];
+                        pDestBuffer[destIndex++] = sourceBuffer[sourceIndex++];
+                        pDestBuffer[destIndex++] = sourceBuffer[sourceIndex++];
+                        pDestBuffer[destIndex++] = sourceBuffer[sourceIndex++];
+                    }
+
+                    destIndex += scanlinePadding;
+                }
+            }
+
+            bitmap.UnlockBits(bitmapData);
+
+            return bitmap;
+        }
+        
+		public static bool ConvertBitmapToImage(ImageBuffer destImage, System.Drawing.Bitmap bitmap)
 		{
 			if (bitmap != null)
 			{
